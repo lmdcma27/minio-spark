676889d93da4d72954e25857f8ee6e4008982d46
lmdcma27 <lmdcma27@gmail.com>
2024-09-22 16:40:39 +0000

    spark_job adjust

--- python-scripts/spark_job2.py
+++ python-scripts/spark_job2.py
@@ -1,22 +1,19 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder \
-    .appName("Spark S3 Example") \
-    .config("spark.hadoop.fs.s3a.access.key", "Nyy86oIKdTOA4HqTAast") \
-    .config("spark.hadoop.fs.s3a.secret.key", "iALsidLdjQbTNh5T3jZI870exdJa6WOiwuPAIm67") \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://192.168.49.2:31662") \
-    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .getOrCreate()
+spark=SparkSession.builder.getOrCreate()
 
+def load_config(spark_context: SparkContext):
 
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.access.key','Nyy86oIKdTOA4HqTAast')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.secret.key','iALsidLdjQbTNh5T3jZI870exdJa6WOiwuPAIm67')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.style.access','true')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.impl','org.apache.hadoop.fs.s3a.S3AFileSystem')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.endpoint','http://127.0.0.1:9000')
+    spark_context._jsc.hadoopConfiguration().set('fs.s3a.connection.ssl.enabled','false')
 
-dataframe = spark.read.json('s3a://orders/company_sales.json')
-
-print('2')
-
-average = dataframe.agg({'sales': 'avg'})
-print('3')
-
+load_config(spark.sparkContext)
+dataframe=spark.read.json('s3a://orders/*')
+dataframe.show()
+average=dataframe.agg({'sales':'avg'})
 average.show()
